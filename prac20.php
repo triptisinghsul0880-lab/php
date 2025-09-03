645c865b012f9bd0db44431b6c08592b8730552c
triptisinghsul0880-lab <triptisinghsul0880@gmail.com>
2025-09-03 21:42:26 +0530

    Add files via upload

---
+++ prac20.php
@@ -0,0 +1,15 @@
+<?php
+ $numbers = array(10,20,30,40,50,60,70,80,90,100);
+echo "Index 0 =>$numbers[0]<br>";
+echo "Index 1 =>$numbers[1]<br>";
+echo "Index 2 =>$numbers[2]<br>";
+echo "Index 3 =>$numbers[3]<br>";
+echo "Index 4 =>$numbers[4]<br>";
+echo "Index 5 =>$numbers[5]<br>";
+echo "Index 6 =>$numbers[6]<br>";
+echo "Index 7 =>$numbers[7]<br>";
+echo "Index 8=>$numbers[8]<br>";
+echo "Index 9=>$numbers[9]<br>";
+
+ 
+?>
